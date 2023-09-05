d92b19acf876286572b09fc7f7062df75182daad
Rady Haggag <radyhaggag500@gmail.com>
2023-09-05 05:05:47 +0300

    Make some refactors

--- app.js
+++ app.js
@@ -9,27 +9,17 @@ const cors = require("cors");
 
 const coursesRouter = require("./routes/courses_routes");
 const httpStatusText = require("./utils/http_status_text");
+const { errorHandlerMiddleware } = require("./middleware/error_handler");
+const notFoundMiddleware = require("./middleware/not_found");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/courses", coursesRouter);
 
-app.use("*", (req, res, next) => {
-  return res.status(404).json({
-    status: httpStatusText.ERROR,
-    message: "This Resource is not available",
-  });
-});
+app.use("*", notFoundMiddleware);
 
-app.use((error, req, res, next) => {
-  console.log(error);
-  res.status(error.statusCode || 500).json({
-    status: error.statusText || httpStatusText.ERROR,
-    message: error.message,
-    data: null,
-  });
-});
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8000;
 
---
+++ middleware/not_found.js
@@ -0,0 +1,10 @@
+const httpStatusText = require("./utils/http_status_text");
+
+const notFoundMiddleware = (req, res, next) => {
+  return res.status(404).json({
+    status: httpStatusText.ERROR,
+    message: "This Resource is not available",
+  });
+};
+
+module.exports = notFoundMiddleware;
